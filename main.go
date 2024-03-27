ab4b4e8f97d889057e78674994e5499fe5ca2e44
Jean Uchoa <jean.uchoa@gmail.com>
2024-03-27 14:19:27 -0300

    move several methods to struct

--- main.go
+++ main.go
@@ -14,11 +14,19 @@ import (
 
 // Config the crawler
 type Config struct {
-	URL            string `toml:"url"`
-	DeckPath       string `toml:"deck_path"`
-	DeckDataPath   string `toml:"deck_data_path"`
-	SideBoardPath  string `toml:"side_board_path"`
-	OtherIndexPath string `toml:"other_indexes_path"`
+	URL               string `toml:"url"`
+	DeckPath          string `toml:"deck_path"`
+	DeckDataPath      string `toml:"deck_data_path"`
+	SideBoardPath     string `toml:"side_board_path"`
+	SideBoardDataPath string `toml:"side_board_data_path"`
+	OtherIndexPath    string `toml:"other_indexes_path"`
+}
+
+type Scraper struct {
+	config Config
+	name   string
+	doc    goquery.Document
+	url    string
 }
 
 func loadConfig(filePath string) (map[string]Config, error) {
@@ -61,71 +69,54 @@ func fetchDeckList(i int, s *goquery.Selection) string {
 	return strings.TrimSpace(s.Text())
 }
 
-func fetchDeckUrls(doc *goquery.Document) {
-	xpath := "div.decks div div table tbody tr td a"
-	deckList := doc.Find(xpath).Map(fetchLink)
+func (scraper *Scraper) fetchDeckUrls() {
+	deckList := scraper.doc.Find(scraper.config.DeckPath).Map(fetchLink)
 
 	fmt.Println(deckList)
 }
 
-func fetchDeckData(doc *goquery.Document) {
-	xpath := "div.wholeDeck td.number"
-	j := doc.Find(xpath).Map(fetchDeckList)
+func (scraper *Scraper) quantityAndCardName(entries []string) map[string]int {
 	myMap := make(map[string]int)
-	for _, line := range j {
+	for _, line := range entries {
 		o := strings.Split(line, "\n")
 		quantity, _ := strconv.Atoi(o[0])
 		myMap[strings.TrimSpace(o[1])] = quantity
 	}
+	return myMap
+}
 
+func (scraper *Scraper) fetchDeckData() {
+	j := scraper.doc.Find(scraper.config.DeckDataPath).Map(fetchDeckList)
+	myMap := scraper.quantityAndCardName(j)
 	fmt.Println(myMap)
 }
 
-func fetchSideboarData(doc *goquery.Document) {
+func (scraper *Scraper) fetchSideboarData() {
 	myMap := make(map[string]int)
-	sideBoardPath := "div.wholeDeck div.row div:last-child .Sideboard"
-	hasSideboard := doc.Find(sideBoardPath).Length() != 0
+	hasSideboard := scraper.doc.Find(scraper.config.SideBoardPath).Length() != 0
 	if hasSideboard {
-		xpath := "div.wholeDeck div.row div:last-child td.number"
-		j := doc.Find(xpath).Map(fetchDeckList)
-		for _, line := range j {
-			o := strings.Split(line, "\n")
-			quantity, _ := strconv.Atoi(o[0])
-			myMap[strings.TrimSpace(o[1])] = quantity
-		}
+		j := scraper.doc.Find(scraper.config.SideBoardDataPath).Map(fetchDeckList)
+		myMap = scraper.quantityAndCardName(j)
 	}
-
 	fmt.Println(myMap)
 }
 
-func fetchOtherIndexLinks(doc *goquery.Document) {
-	xpath := "ul.pagination li a"
-	indexesList := doc.Find(xpath).Map(fetchLink)
+func (scraper *Scraper) fetchOtherIndexLinks() {
+	indexesList := scraper.doc.Find(scraper.config.OtherIndexPath).Map(fetchLink)
 
 	fmt.Println(indexesList)
 }
 
-func fetchIndexPageData(url string) {
-	indexPage, err := fetchPageContent(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Links")
-	fetchOtherIndexLinks(indexPage)
-	fetchDeckUrls(indexPage)
+func (scraper *Scraper) fetchIndexPageData() {
+	scraper.fetchOtherIndexLinks()
+	scraper.fetchDeckUrls()
 }
 
-func fetchDeckPageData(url string) {
-	deckPage, err := fetchPageContent(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+func (scraper *Scraper) fetchDeckPageData() {
 	fmt.Println("Deck")
-	fetchDeckData(deckPage)
+	scraper.fetchDeckData()
 	fmt.Println("Sideboard")
-	fetchSideboarData(deckPage)
+	scraper.fetchSideboarData()
 }
 
 func main() {
@@ -134,10 +125,32 @@ func main() {
 		log.Fatalf("Error reading config file: %s", err)
 	}
 
-	fetchIndexPageData("index-deck-list.html")
-	fetchDeckPageData("deck-page.html")
-	fetchDeckPageData("deck-with-side.html")
+	// doc, err := fetchPageContent(configs["mtgdecks"].URL)
+	doc, err := fetchPageContent("index-deck-list.html")
+	if err != nil {
+		log.Fatalf("Error parsing URL: %s", err)
+	}
 
-	fmt.Println("config")
-	fmt.Println(configs["mtgdecks"].URL)
+	scraper := Scraper{
+		config: configs["mtgdecks"],
+		name:   "mtgdecks",
+		doc:    *doc,
+		url:    configs["mtgdecks"].URL,
+		// url: "index-page.html",
+	}
+
+	scraper.fetchIndexPageData()
+	doc, err = fetchPageContent("deck-page.html")
+	if err != nil {
+		log.Fatalf("Error parsing URL: %s", err)
+	}
+	scraper.doc = *doc
+	scraper.fetchDeckPageData()
+
+	doc, err = fetchPageContent("deck-with-side.html")
+	if err != nil {
+		log.Fatalf("Error parsing URL: %s", err)
+	}
+	scraper.doc = *doc
+	scraper.fetchDeckPageData()
 }
